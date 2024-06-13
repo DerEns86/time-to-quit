8746259af4ee39366608bd6de332cc6788b08cbe
Viktor Ens <v.ens86@yahoo.de>
2024-06-13 10:55:42 +0200

    test push

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,17 +18,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+
         <Button variant="outlined" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
