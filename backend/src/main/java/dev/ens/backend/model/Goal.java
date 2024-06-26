9ac00861e22c414edf6313af597ea8730d91f13d
Viktor Ens <v.ens86@yahoo.de>
2024-06-26 09:48:02 +0200

    remove unused imports

--- backend/src/main/java/dev/ens/backend/model/Goal.java
+++ backend/src/main/java/dev/ens/backend/model/Goal.java
@@ -1,10 +1,6 @@
 package dev.ens.backend.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.Instant;
-
 
 public record Goal(
         String goalId,
