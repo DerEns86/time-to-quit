9bdbe110f452e6425ffa939229e46fa397033633
Viktor Ens <v.ens86@yahoo.de>
2024-06-30 09:25:32 +0200

    added bottom navbar

--- frontend/src/ProtectedRoute.tsx
+++ frontend/src/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {githubUser} from "./model/userModel.ts";
+import Navbar from "./components/shared/Navbar.tsx";
 
 type ProtectedRouteProps = {
     user: githubUser | null | undefined;
@@ -8,10 +9,14 @@ type ProtectedRouteProps = {
 export default function ProtectedRoute(props: ProtectedRouteProps) {
     const isAuthenticated = props.user !== undefined && props.user !== null;
 
-    if (props.user === undefined){
+    if (props.user === undefined) {
         return <div>Loading...</div>
     }
 
-    return isAuthenticated ? <Outlet /> : <Navigate to="/" />
+    return isAuthenticated ? (<>
+            <Outlet/>
+            <Navbar />
+        </>
+    ) : <Navigate to="/"/>
 
 }
