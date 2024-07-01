9e73960dde740bae9d672ae7bd107b092a401258
Viktor Ens <v.ens86@yahoo.de>
2024-07-01 12:09:35 +0200

    update MoneyProgress.tsx

--- frontend/src/components/MoneyProgress.tsx
+++ frontend/src/components/MoneyProgress.tsx
@@ -1,11 +1,24 @@
 import {Box, Paper} from "@mui/material";
 import {getNotSmokedCigarettes, savedMoney, savedMoneyInAYear} from "../utilities/cigaretteUtilities.ts";
 import {githubUser} from "../model/userModel.ts";
+import {useEffect, useState} from "react";
 
 export default function MoneyProgress({ user } : Readonly<{ user: githubUser }>) {
+    const [isTracking, setIsTracking] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (user.quitDate) {
+            setIsTracking(true);
+        }
+    }, [user]);
+
+    
     return (
         <>
             <h4>Statistiken</h4>
+            {!isTracking ? <p>Du hast noch kein Rauchstopp-Datum gesetzt</p>
+                : (
+               <>
             <Paper>
                 <Box>
                     <p>Seit dem Rauchstopp hast du <strong>{getNotSmokedCigarettes(user)}</strong> Zigaretten
@@ -16,6 +29,9 @@ export default function MoneyProgress({ user } : Readonly<{ user: githubUser }>)
             <Paper>
                 <p>In einem Jahr wirst du <strong>{savedMoneyInAYear(user).toFixed(2)}</strong> gespart haben</p>
             </Paper>
+    </>
+)
+}
         </>
     )
 }
