4a049b240e9a741afd04e014dd036e84dc54d459
Viktor Ens <v.ens86@yahoo.de>
2024-07-02 07:40:00 +0200

    style Home.tsx

--- frontend/src/components/styles.ts
+++ frontend/src/components/styles.ts
@@ -11,3 +11,27 @@ export const StyledMoneyPaper = styled(Paper)(({ theme }) => ({
     maxWidth: '400px'
 
 }));
+
+export const StyledMotivationPaperNoActions = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(2),
+    marginInline: '16px',
+    marginBottom: theme.spacing(2),
+    backgroundColor: '#44AA44',
+    color: '#EAEAEA',
+    width: 'calc(100% - 32px)',
+    maxWidth: '400px',
+    textAlign: 'center',
+
+}));
+
+export const StyledGoalPaperNoActions = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(1,2),
+    marginInline: '16px',
+    marginBottom: theme.spacing(2),
+    backgroundColor: '#1A73E8',
+    color: 'white',
+    width: 'calc(100% - 32px)',
+    maxWidth: '400px',
+    textAlign: 'center',
+
+}));
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,8 @@ import {Goal} from "../model/goal.ts";
 import SmokeFreeTracker from "../components/SmokeFreeTracker.tsx";
 import MoneyProgress from "../components/MoneyProgress.tsx";
 import {useState} from "react";
+import {Box, Typography} from "@mui/material";
+import {StyledGoalPaperNoActions, StyledMotivationPaperNoActions} from "../components/styles.ts";
 
 type HomeProps = {
     user: githubUser | null | undefined;
@@ -16,35 +18,59 @@ export default function Home(props: Readonly<HomeProps>) {
 
     return (
         <main>
-            <h1>Willkommen <br/>{props.user?.username}</h1>
-            <div>
-                <SmokeFreeTracker user={props.user as githubUser}
-                                  isTracking={isTracking}
-                                  setIsTracking={setIsTracking}
-                />
-                <MoneyProgress user={props.user as githubUser}
-                               isTracking={isTracking}
-                               setIsTracking={setIsTracking}
-                />
+            <Typography
+                variant={"h4"}
+                className={"text-gray"}
+                sx={{textAlign: "center",
+                    paddingY: 2
+            }}
 
-                <h4>Motivation</h4>
+            >Willkommen {props.user?.username}</Typography>
+
+            <SmokeFreeTracker user={props.user as githubUser}
+                              isTracking={isTracking}
+                              setIsTracking={setIsTracking}
+            />
+            <MoneyProgress user={props.user as githubUser}
+                           isTracking={isTracking}
+                           setIsTracking={setIsTracking}
+            />
+
+            <section>
+                <Typography variant={"subtitle1"} fontWeight={"bold"} className={"text-gray"}>"Warum?"</Typography>
                 {props.user?.mainMotivation.length === 0 ? <div>Du hast noch keine Motivationen gesetzt</div>
-                    : <div>
-                        {props.user?.mainMotivation.map((motivation) => {
-                            return <div
-                                key={motivation}>
-                                {motivation}
-                            </div>
-                        })}
-
-                    </div>
+                    : props.user?.mainMotivation.map((motivation) => {
+                        return <StyledMotivationPaperNoActions
+                            variant={"elevation"}
+                            elevation={8}
+                            key={motivation}>
+                            {motivation}
+                        </StyledMotivationPaperNoActions>
+                    })
                 }
+            </section>
+            <section>
+                <Typography variant={"subtitle1"} fontWeight={"bold"} className={"text-gray"}>"Wofür?"</Typography>
+                {props.goals.length > 0 ?
+                    <>
+                        {props.user?.goals.length === 1 ?
+                            <Typography variant={"body2"} className={"text-gray"} pb={0.5}
+                            >
+                                Du hast dir {props.goals.length} Ziel gesetzt</Typography> :
+                            <Typography variant={"body2"} className={"text-gray"} pb={0.5}
+                            >
+                                Du hast dir {props.goals.length} Ziele gesetzt</Typography>}
+                        {props.goals.map((goal) => {
+                            return <StyledGoalPaperNoActions variant={"elevation"} elevation={8} key={goal.goalId}>
+                                <Typography variant={"body1"}>{goal.goalName}</Typography>
+                                <Typography variant={"body2"} fontWeight={"bold"}>{goal.goalPrice}€</Typography>
+                            </StyledGoalPaperNoActions>
+                        })}
+                    </>
+                    : <Box>Du hast noch keine Ziele festgelegt</Box>}
+            </section>
 
 
-                <h4>Goals</h4>
-                {props.goals.length > 0 ? <div>Du hast dir {props.goals.length} Ziele gesetzt</div>
-                    : <div>Du hast noch keine Ziele gesetzt</div>}
-            </div>
         </main>
     )
 }
