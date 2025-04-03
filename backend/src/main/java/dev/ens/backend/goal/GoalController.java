d82c5666a4837ac13928946658815e4931653219
DerEns86 <v.ens86@yahoo.de>
2025-04-03 16:37:52 +0200

    Remove Swagger/OpenAPI annotations from AuthController and GoalController

--- backend/src/main/java/dev/ens/backend/goal/GoalController.java
+++ backend/src/main/java/dev/ens/backend/goal/GoalController.java
@@ -2,7 +2,6 @@ package dev.ens.backend.goal;
 
 import dev.ens.backend.model.Goal;
 import dev.ens.backend.model.GoalDTO;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,25 +14,21 @@ public class GoalController {
 
     private final GoalService goalService;
 
-    @Operation(summary = "Get user goals", description = "Retrieve a list of goals for a specific user")
     @GetMapping("/{userId}/goals")
     public List<Goal> getGoals(@PathVariable String userId) {
         return goalService.getGoals(userId);
     }
 
-    @Operation(summary = "Add a new goal", description = "Add a new goal for a specific user")
     @PostMapping("/{userId}/goals")
     public Goal addGoal(@PathVariable String userId, @RequestBody Goal goal) {
         return goalService.addGoal(userId, goal);
     }
 
-    @Operation(summary = "Update a goal", description = "Update an existing goal for a specific user")
     @PutMapping("/{userId}/{goalId}")
     public Goal updateGoal(@PathVariable String userId, @PathVariable String goalId, @RequestBody GoalDTO goalDTO) {
         return goalService.updateGoal(userId, goalId, goalDTO);
     }
 
-    @Operation(summary = "Delete a goal", description = "Delete an existing goal for a specific user")
     @DeleteMapping("/{userId}/{goalId}")
     public void deleteGoal(@PathVariable String userId, @PathVariable String goalId) {
         goalService.deleteGoal(userId, goalId);
--- backend/src/main/java/dev/ens/backend/security/AuthController.java
+++ backend/src/main/java/dev/ens/backend/security/AuthController.java
@@ -3,9 +3,6 @@ package dev.ens.backend.security;
 
 import dev.ens.backend.model.AppUser;
 import dev.ens.backend.user.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -21,7 +18,6 @@ public class AuthController {
     private final UserService userService;
 
 
-    @Operation(summary = "Get the authenticated user's details", description = "Returns the details of the authenticated user")
     @GetMapping("/me")
     public AppUser getMe(@AuthenticationPrincipal OAuth2User user) {
         return userService.getUserById(user.getAttributes().get("id").toString());
